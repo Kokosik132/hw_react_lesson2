d58a6468cd930e7244d37049d78f8cb1122da762
Kokosik132 <nbeyshchak@gmail.com>
2022-04-13 17:53:17 +0300

    master

---
+++ src/components/CarForm/CarForm.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import {useForm} from "react-hook-form";
+
+const CarForm = () => {
+    useForm()
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export {CarForm};
---
+++ src/service/car_service.js
@@ -0,0 +1,11 @@
+import {axiosService} from "./axios_service";
+import {urls} from "../constants";
+
+
+export const carService = {
+    getAll: () => axiosService.get(urls.cars),
+    getById: (id) => axiosService.get(`${urls.cars}/${id}`),
+    create:(car) => axiosService.post(urls.cars, car),
+    updateById: (id, newCar) => axiosService.put(`${urls.cars}/${id}`, newCar),
+    deleteById: (id) => axiosService.delete(`${urls.cars}/${id}`)
+}
